524726223c18690850ee3b3cfdff82589acab2d5
flora <xiaorui.zhang@sjsu.edu>
2021-03-19 23:42:46 -0700

    update new front-end

--- new_front/src/helper/user.js
+++ new_front/src/helper/user.js
@@ -8,39 +8,10 @@ export const read = (userId, token) => {
         }
     })
         .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
-};
 
-export const update = (userId, token, user) => {
-    console.log("USER DATA UPDATE: ", user);
-    return fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-        method: "PUT",
-        headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-        },
-        body: user
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
-};
-
-
-export const remove = (userId, token) => {
-    return fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-        method: "DELETE",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-        .then(response => {
-            return response.json();
+            //console.log(JSON.parse(response));
+           return response.json();
+            //return JSON.parse(response);
         })
         .catch(err => console.log(err));
 };
@@ -54,20 +25,6 @@ export const list = () => {
         })
         .catch(err => console.log(err));
 };
-
-export const updateUser = (user, next) => {
-    if (typeof window !== "undefined") {
-        if (localStorage.getItem("jwt")) {
-            let auth = JSON.parse(localStorage.getItem("jwt"));
-            auth.user = user;
-            localStorage.setItem("jwt", JSON.stringify(auth));
-            next();
-        }
-    }
-};
-
-
-
 
 export const follow = (userId, token, followId) => {
     return fetch(`${process.env.REACT_APP_API_URL}/user/follow`, {
@@ -100,19 +57,3 @@ export const unfollow = (userId, token, unfollowId) => {
         })
         .catch(err => console.log(err));
 };
-
-export const findPeople = (userId, token) => {
-    return fetch(`${process.env.REACT_APP_API_URL}/user/findpeople/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
-    })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => console.log(err));
-};
-
